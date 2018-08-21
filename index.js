a9eb0fc7d874a29fc5e6fcedd097bc43280205e1
clairemacdonald <claire.macdonald@blis.com>
2018-08-21 19:51:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var dodger = document.getElementById('dodger');
 
 dodger.style.backgroundColor = 'pink';
 
-dodger.style.left = "180px";
-dodger.style.bottom = "0px";
+
+dodger.style.bottom = "100px";
