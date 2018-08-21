7402e322e86430a59414c49ab0f662326573e5fb
clairemacdonald <claire.macdonald@blis.com>
2018-08-21 20:01:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ document.addEventListener('keydown', function(e) {
   }
     
       if (e.which === 39) {
-    var rightNumbers = dodger.style.right.replace('px', '')
-    var right = parseInt(rightNumbers, 10)
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
  
-    dodger.style.right = `${right - 1}px`
+    dodger.style.left = `${left + 1}px`
   }
 })
